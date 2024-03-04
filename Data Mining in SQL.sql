81dc2cad50f5ae14d19d994ba3546a254d7b42cc
TaNhuThuan <142424814+TaNhuThuan@users.noreply.github.com>
2024-03-04 22:46:51 +0700

    Update Data Mining in SQL.sql

--- Data Mining in SQL.sql
+++ Data Mining in SQL.sql
@@ -1,21 +1,21 @@
 /* Query 01: calculate total visits, page views, and transactions for Jan, Feb, and March 2017 (order by month) */
 
 SELECT  
-  Format_date("%Y%m", parse_date("%Y%m%d", date)) as month  -- Chuyển đổi định dạng 'Date' từ YYYYMMDD sang YYYYMM
+  Format_date("%Y%m", parse_date("%Y%m%d", date)) as month                          -- Chuyển đổi định dạng 'Date' từ YYYYMMDD sang YYYYMM
   
-  sum(totals.visits) as visits,                             -- Tính tổng lượt thăm trang web, gán metric này tên "Visits"
+  sum(totals.visits) as visits,                                                     -- Tính tổng lượt thăm trang web, gán metric này tên "Visits"
   
-  sum(totals.pageviews) as pageviews,                       -- Tính tổng lượt bấm lướt các trang trên trang web, gán metric này tên "pageviews"
+  sum(totals.pageviews) as pageviews,                                               -- Tính tổng lượt bấm lướt các trang trên trang web, gán metric này tên "pageviews"
   
-  sum(totals.transactions) as transactions                  -- Tính tổng lượt giao dịch, gán metric này tên "transactions"
+  sum(totals.transactions) as transactions                                          -- Tính tổng lượt giao dịch, gán metric này tên "transactions"
   
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`
   
-WHERE _table_suffix between '0101' and '0331'               -- Giới hạn data trong khoảng từ tháng 1 đến tháng 3 
+WHERE _table_suffix between '0101' and '0331'                                       -- Giới hạn data trong khoảng từ tháng 1 đến tháng 3 
   
-GROUP BY month                                              -- Nhóm các giá trị trả lại theo tháng
+GROUP BY month                                                                      -- Nhóm các giá trị trả lại theo tháng
         
-ORDER BY month ASC;                                         -- Sắp xếp theo tháng, thứ tự tăng dần (1 -> 3)
+ORDER BY month ASC;                                                                 -- Sắp xếp theo tháng, thứ tự tăng dần (1 -> 3)
 
 
 /* Query 02: Bounce rate per traffic source in July 2017 */
@@ -87,7 +87,7 @@ FROM week_revenue;
 
 /* Query 04: Average number of pageviews by purchaser type (purchasers vs non-purchasers) in June, and July 2017. */
 
-WITH P1 AS (
+WITH P1 AS (                                                                         -- Khởi tạo cte p1 chứa trung bình lượt xem trang và có mua hàng. 
 SELECT  
   FORMAT_DATE('%Y%m', DATETIME (PARSE_DATE('%Y%m%d', date))) as month,
   
@@ -98,7 +98,7 @@ FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`,
 UNNEST (hits) hits,
 UNNEST (hits.product) product
   
-WHERE _table_suffix between '0601' and '0731' and                                    -- Sử dụng Wildcard _table_suffix để 
+WHERE _table_suffix between '0601' and '0731' and                                    -- Sử dụng Wildcard "_table_suffix" để rút gọn câu code (tượng trưng cho phần ký tự có thể thay đổi trong các hàng dữ liệu).
   
       totals.transactions >=1 and
   
@@ -106,7 +106,7 @@ WHERE _table_suffix between '0601' and '0731' and                               
   
 GROUP BY month),
 
-p2 as (
+p2 as (                                                                              -- Khởi tạo cte p2 chứa trung bình lượt xem trang nhưng không mua hàng.
 SELECT  
   
   FORMAT_DATE('%Y%m', DATETIME (PARSE_DATE('%Y%m%d', date))) as month,
@@ -128,12 +128,8 @@ GROUP BY month)
 
 SELECT *
 FROM p1 
-INNER JOIN p2   --> left join/ full join
+LEFT JOIN p2                                                                         -- Sử dụng Left join để join 2 cte p1 và p2 lại với nhau.
 USING (month);
-
---câu 4 này lưu ý là mình nên dùng left join hoặc full join, bởi vì trong câu này, phạm vi chỉ từ tháng 6-7, nên chắc chắc sẽ có pur và nonpur của cả 2 tháng
---mình inner join thì vô tình nó sẽ ra đúng. nhưng nếu đề bài là 1 khoảng thời gian dài hơn, 2-3 năm chẳng hạn, nó cũng tháng chỉ có nonpur mà k có pur
---thì khi đó inner join nó sẽ làm mình bị mất data, thay vì hiện số của nonpur và pur thì nó để trống
 
 /* Query 05: Average number of transactions per user that purchased in July 2017 */
 
@@ -141,7 +137,7 @@ SELECT
   
   FORMAT_DATE('%Y%m', DATETIME (PARSE_DATE('%Y%m%d', date))) as month,
   
-  (SUM(totals.transactions)) / COUNT(distinct fullVisitorId) as Avg_total_transactions_per_user
+  (SUM(totals.transactions)) / COUNT(distinct fullVisitorId) as Avg_total_transactions_per_user        -- Tính trung bình số giao dịch trên mỗi user
   
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
   
@@ -160,7 +156,7 @@ SELECT
   FORMAT_DATE('%Y%m', DATETIME (PARSE_DATE('%Y%m%d', date))) as month,
   
   ROUND(
-    (((SUM(productRevenue)) / COUNT(totals.visits))/1000000),2) as Avg_total_transactions_per_user
+    (((SUM(productRevenue)) / COUNT(totals.visits))/1000000),2) as Avg_Revenue_per_user                -- Tính trung bình Doanh thu trên mỗi user
   
 FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*`,
   
@@ -185,11 +181,11 @@ SELECT
 UNNEST (hits) hits,
 UNNEST (hits.product) product
   
-WHERE v2ProductName = "YouTube Men's Vintage Henley" and
+WHERE v2ProductName = "YouTube Men's Vintage Henley" and                            -- Sau khi Unnest và reach được tới bảng "product", lọc ra những giao dịch hợp lệ với sản phẩm là "YouTube Men's Vintage Henley".
   
-      productRevenue is not null
+      productRevenue is not null                                  
   
-GROUP BY fullVisitorId)
+GROUP BY fullVisitorId)                                                             -- Đến bước này ta sẽ có cte chứa ID của những user mua sp tên "YouTube Men's Vintage Henley".                                                               
 
 SELECT  
   
@@ -202,137 +198,48 @@ FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201707*` as p2,
 UNNEST (hits) hits,
 UNNEST (hits.product) product
   
-LEFT JOIN user1 ON user1.fullVisitorId = p2.fullVisitorId  --> inner join
+INNER JOIN user1 ON user1.fullVisitorId = p2.fullVisitorId                           -- Inner join cte user1 với p2 để tạo ra bảng chứa tên và số lương các sản phẩm được mua khi người mua đã mua sản phẩm "YouTube Men's Vintage Henley".
   
 WHERE productRevenue is not null
   
 GROUP BY v2ProductName;
---> ở phần này thì mình nên dùng inner join, bởi vì mình chỉ muốn lấy tập giao của fullvisitorId thôi 
 
---Query 08: Calculate cohort map from product view to addtocart to purchase in Jan, Feb and March 2017.
+/* Query 08: Calculate cohort map from product view to add to cart to purchase in Jan, Feb, and March 2017. 
 
-with view1 as(
+hits.eCommerceAction.action_type = '2' is view product page; 
+hits.eCommerceAction.action_type = '3' is add to cart; 
+hits.eCommerceAction.action_type = '6' is purchase
+
+*/
+
+WITH product_data as(
 SELECT
-  FORMAT_DATE('%Y%m', DATETIME (PARSE_DATE('%Y%m%d', date))) as month,
-  COUNT(eCommerceAction.action_type) as num_product_view
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`,
-UNNEST (hits) hits
-WHERE _table_suffix between '0101' and '0331'
-      and eCommerceAction.action_type = '2'
-GROUP BY month),
-
-addtocart as(
-SELECT
-  FORMAT_DATE('%Y%m', DATETIME (PARSE_DATE('%Y%m%d', date))) as month,
-  COUNT(eCommerceAction.action_type) as num_addtocart
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`,
-UNNEST (hits) hits
-WHERE _table_suffix between '0101' and '0331'
-      and eCommerceAction.action_type = '3'
-GROUP BY month),
-
-purchase as(
-SELECT
-  FORMAT_DATE('%Y%m', DATETIME (PARSE_DATE('%Y%m%d', date))) as month,
-  COUNT(eCommerceAction.action_type) as num_purchase
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_2017*`,
-UNNEST (hits) hits,
-UNNEST (hits.product) product
-WHERE _table_suffix between '0101' and '0331'
-      and eCommerceAction.action_type = '6'
-      and productRevenue is not null
-GROUP BY month)
-
-SELECT 
-  month,
-  num_product_view,
-  num_addtocart,
-  num_purchase,
-  ROUND((100 *(num_addtocart/num_product_view)),2) as add_to_cart_rate,
-  ROUND((100 *(num_purchase/num_product_view)),2) as purchase_rate
-FROM view1
-INNER JOIN addtocart USING (month)
-INNER JOIN purchase USING (month)
-GROUP BY month,num_product_view,num_addtocart,num_purchase
-ORDER BY month;
-
---bài yêu cầu tính số sản phầm, mình nên count productName hay productSKU thì sẽ hợp lý hơn là count action_type
---k nên xài inner join, nếu table1 có 10 record,table2 có 5 record,table3 có 1 record, thì sau khi inner join, output chỉ ra 1 record
-
---Cách 1:dùng CTE
-with
-product_view as(
-SELECT
-  format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
-  count(product.productSKU) as num_product_view
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-, UNNEST(hits) AS hits
-, UNNEST(hits.product) as product
-WHERE _TABLE_SUFFIX BETWEEN '20170101' AND '20170331'
-AND hits.eCommerceAction.action_type = '2'
-GROUP BY 1
-),
-
-add_to_cart as(
-SELECT
-  format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
-  count(product.productSKU) as num_addtocart
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-, UNNEST(hits) AS hits
-, UNNEST(hits.product) as product
-WHERE _TABLE_SUFFIX BETWEEN '20170101' AND '20170331'
-AND hits.eCommerceAction.action_type = '3'
-GROUP BY 1
-),
-
-purchase as(
-SELECT
-  format_date("%Y%m", parse_date("%Y%m%d", date)) as month,
-  count(product.productSKU) as num_purchase
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-, UNNEST(hits) AS hits
-, UNNEST(hits.product) as product
-WHERE _TABLE_SUFFIX BETWEEN '20170101' AND '20170331'
-AND hits.eCommerceAction.action_type = '6'
-and product.productRevenue is not null   --phải thêm điều kiện này để đảm bảo có revenue
-group by 1
-)
-
-select
-    pv.*,
-    num_addtocart,
-    num_purchase,
-    round(num_addtocart*100/num_product_view,2) as add_to_cart_rate,
-    round(num_purchase*100/num_product_view,2) as purchase_rate
-from product_view pv
-left join add_to_cart a on pv.month = a.month
-left join purchase p on pv.month = p.month
-order by pv.month;
-
---bài này k nên inner join, vì nếu như bảng purchase k có data thì sẽ k mapping đc vs bảng productview, từ đó kết quả sẽ k có luôn, mình nên dùng left join
-
---Cách 2: bài này mình có thể dùng count(case when) hoặc sum(case when)
-
-with product_data as(
-select
+  
     format_date('%Y%m', parse_date('%Y%m%d',date)) as month,
-    count(CASE WHEN eCommerceAction.action_type = '2' THEN product.v2ProductName END) as num_product_view,
-    count(CASE WHEN eCommerceAction.action_type = '3' THEN product.v2ProductName END) as num_add_to_cart,
+  
+    count(CASE WHEN eCommerceAction.action_type = '2' THEN product.v2ProductName END) as num_product_view,                                      -- Dùng "count(CASE WHEN)" để phân loại và đếm số lượng các action_type:
+                                                                                                                                                -- "product_view, add_to_cart and purchase"
+    count(CASE WHEN eCommerceAction.action_type = '3' THEN product.v2ProductName END) as num_add_to_cart,                                    
+  
     count(CASE WHEN eCommerceAction.action_type = '6' and product.productRevenue is not null THEN product.v2ProductName END) as num_purchase
-FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`
-,UNNEST(hits) as hits
-,UNNEST (hits.product) as product
-where _table_suffix between '20170101' and '20170331'
-and eCommerceAction.action_type in ('2','3','6')
-group by month
-order by month
+  
+FROM `bigquery-public-data.google_analytics_sample.ga_sessions_*`,
+  
+UNNEST(hits) as hits,
+UNNEST (hits.product) as product
+  
+WHERE _table_suffix between '20170101' and '20170331'                                                                                           -- Lọc dữ liệu từ tháng 1/2017 đến hết tháng 3/2017 và 
+                                                                                                                                                -- filter thêm action_type 1 lần nữa để chắc chắn.
+and eCommerceAction.action_type in ('2','3','6')                                      
+  
+GROUP BY month
+ORDER BY month
 )
 
 select
     *,
-    round(num_add_to_cart/num_product_view * 100, 2) as add_to_cart_rate,
-    round(num_purchase/num_product_view * 100, 2) as purchase_rate
+    round(num_add_to_cart/num_product_view * 100, 2) as add_to_cart_rate,                                                                       -- Tính tỷ lệ add to cart trên số lần xem sản phẩm 
+  
+    round(num_purchase/num_product_view * 100, 2) as purchase_rate                                                                              -- và tỷ lệ mua hàng trên số lần xem sản phẩm 
+  
 from product_data;
-
-
-                                                          ---very good---
